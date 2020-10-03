5193bca739ed6f0f826851a03ff6297a52ff5cb8
melissikiss <melissikiss@gmail.com>
2020-10-03 13:56:20 -0700

    final generator

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
 // Assignment code here
-var lettersLC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var lettersUC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialCharacters = ["!", "#", "$", "%", "*"];
-
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "1234567890";
+var specialChar = "!#%*";
 function generatePassword() {
+
+  // length
   var length = window.prompt("Enter the number of characters you want for your new password! It must be between 8 - 128.");
-  if (length <8 || length >128) {
+  if (length < 8 || length > 128) {
     window.alert("Amount not acceptable. Please try again, between 8 and 128!");
     return generatePassword();
-  } 
-  var lowercase = window.prompt("Do you want lowercases in your password?");
-  if (lowercase === "yes" || lowercase === "YES") {
-    }
-  else {
-    lowercase = null;
+  }
+  var characters = "";
+  // lowercase
+  var lowercase = window.confirm("Do you want lowercases in your password?");
+  if (lowercase) {
+    characters = characters + lowercaseChar;
   }
   console.log(lowercase);
 
-  var uppercase = window.prompt("Do you want uppercases in your password?");
-  if (uppercase === "yes" || uppercase === "YES") {
-
-  }
-  else {
-    uppercase = null;
+  // uppercase
+  var uppercase = window.confirm("Do you want uppercases in your password?");
+  if (uppercase) {
+characters = characters + uppercaseChar;
   }
   console.log(uppercase);
 
-  var numbers = window.prompt("Do you want numbers in your password?");
-  if (numbers === "yes" || numbers === "YES") {
-    numbers = (Math.floor(Math.random() * 10));
-   } 
-  else {
-    numbers = null;
+  // numbers
+  var numbers = window.confirm("Do you want numbers in your password?");
+  if (numbers) {
+    characters = characters + numberChar;
   }
   console.log(numbers);
 
-  var special = window.prompt("Do you want special characters in your password?");
-  if (special === "yes" || special === "YES") {
-    
-  }
-  else {
-    special = null;
+  // special characters
+  var special = window.confirm("Do you want special characters in your password?");
+  if (special) {
+    characters = characters + specialChar;
   }
   console.log(special);
-
+  console.log(characters);
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    console.log(length);
+    var randomNumber = (Math.floor(Math.random() * characters.length));
+    password = password + characters[randomNumber];
+  }
+  console.log(password);
+  return password;
 }
 
-  
+
 
 
 
